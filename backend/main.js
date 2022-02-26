7b89666cd279713449ec939d042e3b3b0aa1fdc3
LyndonFan <lyndon0808@gmail.com>
2022-02-26 23:50:28 +0000

    multiple channels

--- backend/main.js
+++ backend/main.js
@@ -5,22 +5,37 @@ var fs = require('fs');
 var readline = require('readline');
 var { google } = require('googleapis');
 
+function getChannelInfo() {
+    return JSON.parse(fs.readFileSync(path.join(__dirname, 'channel_names.json')));
+}
+
 function main() {
     const RESULTFILEPATH = path.join(__dirname, 'videos.json');
-    return (async () => {
+    const channelInfo = getChannelInfo();
+    fs.writeFileSync(RESULTFILEPATH, '[]');
+    const f = (async (channelId) => {
         try {
             const auth = await authenticate.authorize();
-            const pid = await search.getPlaylistId(auth, "", 'UCYO_jab_esuFRV4b17AJtAw');
+            if (auth == undefined) { throw new Error('Authentication failed.'); }
+            const pid = await search.getPlaylistId(auth, channelId);
+            if (pid == undefined) { throw new Error('Playlist ID not found.'); }
             var videoIds = await search.getVideoIds(auth, pid);
+            if (videoIds == undefined) { throw new Error('Video IDs not found.'); }
             videoIds = videoIds.map((video) => video.contentDetails.videoId);
             const videos = await search.getVideos(auth, videoIds);
+            if (videos == undefined) { throw new Error('Videos not found.'); }
             console.log('%s video(s) found', videos.length);
-            fs.writeFileSync(RESULTFILEPATH, JSON.stringify(videos));
+            const res = JSON.parse(fs.readFileSync(RESULTFILEPATH));
+            res.push(...videos);
+            fs.writeFileSync(RESULTFILEPATH, JSON.stringify(res));
             console.log('Results saved at', RESULTFILEPATH);
         } catch (err) {
             console.log(err);
+            throw err;
         }
-    })();
+    });
+    channelInfo.forEach(x => { console.log("Grabbing", x.channelName, "..."); f(x.channelId); });
+    return true;
 }
 
 main();

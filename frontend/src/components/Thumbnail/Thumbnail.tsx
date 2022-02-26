d798690c666b22ab52339c24e2535658a141cff9
Salman <salmanfaris98@gmail.com>
2022-02-26 19:50:52 +0000

    Thumbnail progress added tags, title

--- frontend/src/components/Thumbnail/Thumbnail.tsx
+++ frontend/src/components/Thumbnail/Thumbnail.tsx
@@ -2,7 +2,20 @@ import "bulma/css/bulma.min.css";
 import "./Thumbnail.css";
 
 const Thumbnail = () => {
-  return <div id="rectangle" className="thumbnail"></div>;
+  return (
+    <>
+      <div className="container video">
+        <div className="thumbnail"></div>
+        <div className="has-text-weight-bold">Hamiltonian Monte Carlo</div>
+        <div>Khan Academy</div>
+        <div className="tags" style={{ margin: "20px;" }}>
+          <span className="tag is-rounded is-light">statistics</span>
+          <span className="tag is-rounded is-light">sampling</span>
+          <span className="tag is-rounded is-light">langevin dynamics</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Thumbnail;

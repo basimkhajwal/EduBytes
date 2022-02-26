f8e202d31f56bfee32135b2d09bb613cd1c7d347
basimkhajwal <basimkhajwal@gmail.com>
2022-02-26 16:24:29 +0000

    Video view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import videos from "./data/videos.json";
+import VideoView from "./components/VideoView";
 
 const App = () => {
   return (
-    <section className="hero is-primary">
-      <div className="container is-primary">
-        <h1 className="title">
-          Hello World
-        </h1>
+    <section className="hero">
+      <div className="container">
+        <h1 className="title">Hello World</h1>
         <p className="subtitle">
           My first website with <strong>Bulma</strong>!
         </p>
+        <VideoView video={videos[0]} />
       </div>
     </section>
   );
-}
+};
 
 export default App;

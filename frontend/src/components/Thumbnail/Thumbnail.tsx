d58b9260bcfb6f7e63542204755810bf72b06c3c
Salman <salmanfaris98@gmail.com>
2022-02-26 20:00:14 +0000

    Thumbnail fixed overflow content issue

--- frontend/src/components/Thumbnail/Thumbnail.tsx
+++ frontend/src/components/Thumbnail/Thumbnail.tsx
@@ -6,12 +6,15 @@ const Thumbnail = () => {
     <>
       <div className="container video">
         <div className="thumbnail"></div>
-        <div className="has-text-weight-bold">Hamiltonian Monte Carlo</div>
+        <div className="has-text-weight-bold">
+          Stochastic gradient Hamiltonian Monte Carlo
+        </div>
         <div>Khan Academy</div>
-        <div className="tags" style={{ margin: "20px;" }}>
+        <div className="tags container pt-2">
           <span className="tag is-rounded is-light">statistics</span>
           <span className="tag is-rounded is-light">sampling</span>
           <span className="tag is-rounded is-light">langevin dynamics</span>
+          <span className="tag is-rounded is-light">mars bars</span>
         </div>
       </div>
     </>

d2dd780473ade7424d9b989a2f4df7c64555cf3b
Salman <salmanfaris98@gmail.com>
2022-02-26 16:51:29 +0000

    Landing page with text and search box

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,28 @@ import "./App.css";
 
 const App = () => {
   return (
-    // <section className="hero is-primary">
-    //   <div className="container is-primary">
-    //     <h1 className="title">Hello World</h1>
-    //     <p className="subtitle">
-    //       My first website with <strong>Bulma</strong>!
-    //     </p>
-    //   </div>
-    // </section>
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div className="container landing-comp">
+        <div className="is-size-1">
+          <h1 className="has-text-weight-bold">What do you want to learn?</h1>
+        </div>
+        <div className="field is-grouped search-box">
+          <p className="control is-expanded">
+            <input
+              className="input"
+              type="text"
+              placeholder="Find a repository"
+            />
+          </p>
+          <p className="control">
+            <a className="button is-info">Search</a>
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 

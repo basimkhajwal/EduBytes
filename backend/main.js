c11e53a1e39b24e42da3222807e92203cb525b85
LyndonFan <lyndon0808@gmail.com>
2022-02-26 23:08:50 +0000

    backend

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,26 @@
 var search = require('./search');
-var auth = require('./auth');
+var path = require('path');
+var authenticate = require('./authenticate');
 var fs = require('fs');
 var readline = require('readline');
 var { google } = require('googleapis');
 
+function main() {
+    const RESULTFILEPATH = path.join(__dirname, 'videos.json');
+    return (async () => {
+        try {
+            const auth = await authenticate.authorize();
+            const pid = await search.getPlaylistId(auth, "", 'UCYO_jab_esuFRV4b17AJtAw');
+            var videoIds = await search.getVideoIds(auth, pid);
+            videoIds = videoIds.map((video) => video.contentDetails.videoId);
+            const videos = await search.getVideos(auth, videoIds);
+            console.log('%s video(s) found', videos.length);
+            fs.writeFileSync(RESULTFILEPATH, JSON.stringify(videos));
+            console.log('Results saved at', RESULTFILEPATH);
+        } catch (err) {
+            console.log(err);
+        }
+    })();
+}
+
+main();

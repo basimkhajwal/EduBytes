c5124e457f0b568995a1e35f66f9fb11f2c193b7
basimkhajwal <basimkhajwal@gmail.com>
2022-02-27 00:18:05 +0000

    Basic search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,13 +14,19 @@ type PageState = { type: "main" } | { type: "video"; video: Video };
 const App = () => {
   const [state, setState] = React.useState<PageState>({ type: "main" });
 
+  const onSearch = (query: String) => {
+    const keyWords = query.toLowerCase().split(" ");
+    function videoScore(video: Video) {
+      return keyWords.filter(
+        (word) => video.snippet.description.toLowerCase().indexOf(word) !== -1
+      ).length;
+    }
+    videos.sort((a, b) => videoScore(b) - videoScore(a));
+    setState({ type: "video", video: videos[0] });
+  };
+
   return state.type === "main" ? (
-    <MainView
-      onSearch={(query) => {
-        console.log(query);
-        setState({ type: "video", video: videos[0] });
-      }}
-    />
+    <MainView onSearch={onSearch} />
   ) : (
     <VideoView video={state.video} />
   );

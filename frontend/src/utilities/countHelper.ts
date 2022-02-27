bf325ab3fd3fa939a03d18bb84e7613d697378f3
LyndonFan <lyndon0808@gmail.com>
2022-02-27 09:59:20 +0000

    fix formatCounts

--- frontend/src/utilities/countHelper.ts
+++ frontend/src/utilities/countHelper.ts
@@ -1,4 +1,4 @@
-export const formatCounts = (numString: string) => {
+export const formatCounts = (numString: string | undefined) => {
   const formatter = Intl.NumberFormat("en", { notation: "compact" });
-  return formatter.format(Number(numString));
+  return formatter.format(Number(numString) ?? 0);
 };

3ddc07ed2945cc1f685e1940f173e8c2ce0f762b
LyndonFan <lyndon0808@gmail.com>
2022-02-27 07:35:45 +0000

    improved videoview?

--- frontend/src/views/VideoView.tsx
+++ frontend/src/views/VideoView.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import "./VideoView.css";
 import Video from "../models/Video";
 import Navbar from "../components/Navbar/Navbar";
-import { Card, CardMedia, Container, Fab, Grid, Stack, Typography } from '@mui/material';
+import { Card, CardMedia, Container, Divider, Fab, Grid, Stack, Typography } from '@mui/material';
 export interface Props {
   video: Video;
   onSearch: (query: string) => void;
   backHome: () => void;
 }
+
 const View = (props: Props) => {
   const {
     id,
-    snippet: { title, description, channelTitle, tags, thumbnails },
+    snippet: { title, description, channelTitle, tags },
+    statistics: { viewCount, likeCount },
   } = props.video;
   const formattedDescription = description
     .split("\n")
     .map((str) => <p>{str}</p>);
+  console.log(formattedDescription);
   const joinedTags = tags === undefined ? "" : tags.join(", ");
   return (
     <>
@@ -23,39 +26,55 @@ const View = (props: Props) => {
       <br />
       <Stack spacing={3} sx={{ maxHeight: "100%", overflow: "auto" }}>
         <Typography component="h1" className="title is-2 block has-text-centered">{title}</Typography>
-        <Card component="div" className="playerContainer">
+        <Card component="div" className="block playerContainer">
           <CardMedia
             component="iframe"
             src={`https://www.youtube.com/embed/${id}?autoplay=1`}
             title="embedding"
             id="embedding"
             sx={{
+              display: "block",
               frameBorder: 0,
               allow: "autoplay; picture-in-picture; fullscreen",
               sandbox: "allow-same-origin allow-scripts",
-              width: window.innerWidth,
-              height: window.innerWidth * 0.5625,
+              width: window.innerWidth - 20,
+              height: (window.innerWidth - 20) * 0.5625,
             }}
           />
         </Card>
-        <Typography component="div" className="is-size-7">{channelTitle}</Typography>
         <Grid container spacing={3}>
+          <Grid item xs="auto">
+            <Typography component="div" className="is-size-4">{"Channel: " + channelTitle}</Typography>
+          </Grid>
+          <Grid item xs="auto">
+            <Typography component="div" className="is-size-4">{viewCount + " views"}</Typography>
+          </Grid>
+          <Grid item xs="auto">
+            <Typography component="div" className="is-size-4">{likeCount + " likes"}</Typography>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs="auto">Tags:</Grid>
           {tags === undefined ? (<></>) : tags.map((tag: string) => {
             return <Grid item xs="auto"><Fab className="tag is-link is-light"
               size="small" onClick={() => props.onSearch(tag)}
               variant="extended" aria-label="add" sx={{ textTransform: "none" }}>{tag}</Fab></Grid>;
           })}
         </Grid>
-        <Container maxWidth="sm">
-          <Stack spacing={3}>
-            <Typography component="h6" className="title is-6 block has-text-centered">Description</Typography>
-            <Typography component="div" className="is-size-7">{formattedDescription}</Typography>
-          </Stack>
-          <Stack spacing={3}>
-            <Typography component="h6" className="title is-6 block has-text-centered">Relevant Links</Typography>
-            <Typography component="div" className="is-size-7">{joinedTags}</Typography>
-          </Stack>
-        </Container>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <Stack spacing={3}>
+              <Typography component="h4" className="title is-4 block has-text-centered">Description</Typography>
+              <Typography component="div" className="is-size-6">{formattedDescription}</Typography>
+            </Stack>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Stack spacing={3}>
+              <Typography component="h4" className="title is-4 block has-text-centered">Relevant Links</Typography>
+              <Typography component="div" className="is-size-6">{joinedTags}</Typography>
+            </Stack>
+          </Grid>
+        </Grid>
       </Stack>
     </>
   );

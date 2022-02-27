b9f620ed0f0c638794ac635d8f3f25d1e6ce9212
LyndonFan <lyndon0808@gmail.com>
2022-02-27 05:34:14 +0000

    attempt at large video

--- frontend/src/views/VideoView.tsx
+++ frontend/src/views/VideoView.tsx
@@ -1,42 +1,63 @@
 import React from "react";
-
 import "./VideoView.css";
-
 import Video from "../models/Video";
 import Navbar from "../components/Navbar/Navbar";
-
+import { Card, CardMedia, Container, Fab, Grid, Stack, Typography } from '@mui/material';
 export interface Props {
   video: Video;
+  onSearch: (query: string) => void;
   backHome: () => void;
 }
-
 const View = (props: Props) => {
   const {
     id,
-    snippet: { title, description },
+    snippet: { title, description, channelTitle, tags, thumbnails },
   } = props.video;
   const formattedDescription = description
     .split("\n")
     .map((str) => <p>{str}</p>);
+  const joinedTags = tags === undefined ? "" : tags.join(", ");
   return (
     <>
       <Navbar backHome={props.backHome} />
-      <div className="box has-text-centered">
-        <div className="block">
-          <iframe
+      <br />
+      <Stack spacing={3} sx={{ maxHeight: "100%", overflow: "auto" }}>
+        <Typography component="h1" className="title is-2 block has-text-centered">{title}</Typography>
+        <Card component="div" className="playerContainer">
+          <CardMedia
+            component="iframe"
+            src={`https://www.youtube.com/embed/${id}?autoplay=1`}
             title="embedding"
             id="embedding"
-            src={"http://www.youtube.com/embed/" + id}
-            frameBorder={0}
-            allowFullScreen
-          ></iframe>
-        </div>
-        <h3 className="title is-4 block">{title}</h3>
-        <h6 className="title is-6 block">Descriptions</h6>
-        <div className="is-size-7">{formattedDescription}</div>
-      </div>
+            sx={{
+              frameBorder: 0,
+              allow: "autoplay; picture-in-picture; fullscreen",
+              sandbox: "allow-same-origin allow-scripts",
+              width: window.innerWidth,
+              height: window.innerWidth * 0.5625,
+            }}
+          />
+        </Card>
+        <Typography component="div" className="is-size-7">{channelTitle}</Typography>
+        <Grid container spacing={3}>
+          {tags === undefined ? (<></>) : tags.map((tag: string) => {
+            return <Grid item xs="auto"><Fab className="tag is-link is-light"
+              size="small" onClick={() => props.onSearch(tag)}
+              variant="extended" aria-label="add" sx={{ textTransform: "none" }}>{tag}</Fab></Grid>;
+          })}
+        </Grid>
+        <Container maxWidth="sm">
+          <Stack spacing={3}>
+            <Typography component="h6" className="title is-6 block has-text-centered">Description</Typography>
+            <Typography component="div" className="is-size-7">{formattedDescription}</Typography>
+          </Stack>
+          <Stack spacing={3}>
+            <Typography component="h6" className="title is-6 block has-text-centered">Relevant Links</Typography>
+            <Typography component="div" className="is-size-7">{joinedTags}</Typography>
+          </Stack>
+        </Container>
+      </Stack>
     </>
   );
 };
-
 export default View;
